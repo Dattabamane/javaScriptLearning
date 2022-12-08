4d4f99c72abac16433934595e16dbf774e5ebeb7
Dattabamane <dattatraybamane1805@gmail.com>
2022-12-08 19:00:31 +0530

    modified variable
    modified variable

--- 01_variable.js
+++ 01_variable.js
@@ -9,9 +9,10 @@ var myLove = "I LOVE JAVASCRIPT";
 console.log(myLove);
 
 var myName = "Dattatray Bamane";
+var birthDate = "04/09/1991"
 var age = "26";
 var address = "pandharpur";
 var pin_code = "413304";
 var gender = "Male";
 var is_married = "no";
-console.log(myName,age,address,pin_code,gender,is_married);
+console.log(myName, birthDate,age,address,pin_code,gender,is_married);
