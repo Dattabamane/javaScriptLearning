a7da1204b438552257c9273b95887f2ecf5d9e9a
Dattabamane <dattatraybamane1805@gmail.com>
2022-12-08 18:15:05 +0530

    variable
    variable

---
+++ 01_variable.js
@@ -0,0 +1,17 @@
+var myName;
+myName = "Dattatray Bamane";
+console.log(myName);
+
+myFullName = "Dattatray Uttam Bamane";
+console.log(myFullName);
+
+var myLove = "I LOVE JAVASCRIPT";
+console.log(myLove);
+
+var myName = "Dattatray Bamane";
+var age = "26";
+var address = "pandharpur";
+var pin_code = "413304";
+var gender = "Male";
+var is_married = "no";
+console.log(myName,age,address,pin_code,gender,is_married);
