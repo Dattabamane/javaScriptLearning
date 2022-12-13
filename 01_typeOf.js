77e43d8a4723da0b4b4a7049173520fdc10c799f
Dattabamane <dattatraybamane1805@gmail.com>
2022-12-13 06:51:49 +0530

    typeof
    typeof

---
+++ 01_typeOf.js
@@ -0,0 +1,32 @@
+console.log("====Before Initialization====");
+var bankName;
+console.log(bankName);
+
+console.log("===After Initialization===");
+
+var bankName = "SBI Bank";
+console.log(bankName);
+console.log(typeof bankName);
+
+console.log("-------------------------------------");
+
+var marks = "90%";
+console.log(marks);
+console.log(typeof marks);
+
+console.log("-------------------------------------");
+
+var isWorking = true;
+console.log(typeof isWorking);
+
+console.log("------------------------------------");
+
+var totalCount = "hundred and seven";
+console.log(totalCount);
+console.log(typeof totalCount);
+
+console.log("------Update Variable------");
+
+var totalCount = 107;
+console.log(totalCount);
+console.log(typeof totalCount);
