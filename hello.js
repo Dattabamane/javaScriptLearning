a516d6ccc88ce64617da658d5c421e1c5820b592
Dattabamane <dattatraybamane1805@gmail.com>
2022-12-13 06:48:21 +0530

    hello
    hello

---
+++ hello.js
@@ -0,0 +1,7 @@
+var myName;  // variable declaration
+console.log(myName);
+ var myName = "Hello Datta"; // variable initialization
+ console.log(myName);
+ var myFullName = "Dttatray Uttam Bamane"; // variable declaration ane intialization
+ console.log(myFullName);
+
