5e12d456bfa2af7f43684e00307f15b6da305bc9
Dattabamane <dattatraybamane1805@gmail.com>
2022-12-20 21:17:58 +0530

    switch statement
    switch statement

---
+++ 01_switchStatement.js
@@ -0,0 +1,62 @@
+console.log("==================================");
+function monthYear(monthNumber){
+      switch(monthNumber){
+        case 1:
+            console.log(monthNumber, "This is January Month");
+            break;
+
+            case 2:
+            console.log(monthNumber, "This is February Month");
+            break;
+
+            case 3:
+            console.log(monthNumber, "This is March Month");
+            break;
+
+            case 4:
+            console.log(monthNumber, "This is April Month");
+            break;
+
+            case 5:
+            console.log(monthNumber, "This is May Month");
+            break;
+
+            case 6:
+            console.log(monthNumber, "This is Jun Month");
+            break;
+
+            case 7:
+            console.log(monthNumber, "This is July Month");
+            break;
+
+            case 8:
+            console.log(monthNumber, "This is August Month");
+            break;
+
+            case 9:
+            console.log(monthNumber, "This is September Month");
+            break;
+
+            case 10:
+            console.log(monthNumber, "This is october Month");
+            break;
+
+            case 11:
+            console.log(monthNumber, "This is November Month");
+            break;
+
+            case 12:
+            console.log(monthNumber, "This is December Month");
+            break;
+    
+        default:
+            console.log(monthNumber, " valid months provided");
+            break;
+    } 
+}  
+    monthYear(1);
+    monthYear(2);
+    monthYear(7);
+    monthYear(12);
+    monthYear(15);
+
