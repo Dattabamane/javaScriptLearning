72ca525c7282bede5dcd9b3b032e86bd5e0653c1
Dattabamane <dattatraybamane1805@gmail.com>
2022-12-22 11:39:21 +0530

    01 Do While Loop
    01 Do While Loop

---
+++ 01_doWhileLoop.js
@@ -0,0 +1,49 @@
+console.log("============== Do Whil Loop===============");
+console.log("==============whil Loop 1. ===============");
+var index =5;
+do {
+    console.log(index);
+    index=index+1;
+} while (index<=15);
+
+console.log("==============whil Loop 2. ===============");
+var index=50;
+do {
+    console.log(index);
+    index=index=index-1;
+} while (index>=40)
+
+console.log("==============whil Loop 3. ===============");
+var index=1;
+do {
+    console.log(index);
+    index=index=index+2;
+} while (index<=15)
+
+console.log("==============whil Loop 4. ===============");
+var index=2;
+do {
+    console.log(index);
+    index=index=index+2;
+} while (index<=10)
+
+console.log("==============whil Loop 5. ===============");
+var index=5;
+do {
+    console.log(index);
+    index=index=index+5;
+} while (index<=50)
+
+console.log("==============whil Loop 6. ===============");
+var index=10;
+do {
+    console.log(index);
+    index=index=index+10;
+} while (index<=100)
+
+console.log("==============whil Loop 7. ===============");
+var index=100;
+do {
+    console.log(index);
+    index=index=index-10;
+} while (index>=10)
