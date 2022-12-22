f0f5b674c57e80eb815c531dae51c42c94e5c237
Dattabamane <dattatraybamane1805@gmail.com>
2022-12-22 11:07:33 +0530

    for statement
    for statement

---
+++ 02_forStatement.js
@@ -0,0 +1,35 @@
+console.log("================loop 1 incrementing====================");
+for (let index = 0; index <=15; index=index+1) {
+    console.log(index);    
+}
+
+console.log("================loop 2 decrementing====================");
+for (let index = 50; index>=40; index=index-1) {
+    console.log(index);    
+}
+
+console.log("================loop 3 decrementing====================");
+for (let index = 1; index<=15; index=index+2) {
+    console.log(index);    
+}
+
+console.log("================loop 4 decrementing====================");
+for (let index =2; index<=10; index=index+2) {
+    console.log(index);    
+}
+
+
+console.log("================loop 5 ====================");
+for (let index = 5; index<=50; index=index+5) {
+    console.log(index);    
+}
+
+console.log("================loop 6 ====================");
+for (let index = 10; index<=100; index=index+10) {
+    console.log(index);    
+}
+
+console.log("================loop 7 ====================");
+for (let index = 100; index>=10; index=index-10) {
+    console.log(index);    
+}
