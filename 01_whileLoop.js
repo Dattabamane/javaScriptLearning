c74bf18f2ac5c07f4c3ea4a397ca5bf24c3f8589
Dattabamane <dattatraybamane1805@gmail.com>
2022-12-22 11:26:03 +0530

    01 while loop
    01 while loop

---
+++ 01_whileLoop.js
@@ -0,0 +1,51 @@
+console.log("==============whil Loop===============");
+console.log("==============whil Loop 1. ===============");
+var index=5;
+while (index<=15) { 
+    console.log(index); 
+    index=index+1; 
+}
+
+console.log("==============whil Loop 2. ===============");
+var index=50;
+while (index>=40) { 
+    console.log(index); 
+    index=index-1; 
+}
+
+console.log("==============whil Loop 3. ===============");
+var index=1;
+while (index<=15) { 
+    console.log(index); 
+    index=index+2; 
+}
+
+console.log("==============whil Loop 4. ===============");
+var index=2;
+while (index<=10) { 
+    console.log(index); 
+    index=index+2; 
+}
+
+console.log("==============whil Loop 5. ===============");
+var index=5;
+while (index<=50) { 
+    console.log(index); 
+    index=index+5; 
+}
+
+console.log("==============whil Loop 6. ===============");
+var index=10;
+while (index<=100) { 
+    console.log(index); 
+    index=index+10; 
+}
+
+console.log("==============whil Loop 7. ===============");
+var index=100;
+while (index>=10) { 
+    console.log(index); 
+    index=index-10; 
+}
+
+
