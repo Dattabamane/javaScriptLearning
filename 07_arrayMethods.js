dae69ea3b4c3c5ad88b5ce46981bb6de8881188d
Dattabamane <dattatraybamane1805@gmail.com>
2022-12-27 21:13:27 +0530

    07_arrayMethods modified
    07_arrayMethods modified

--- 07_arrayMethods.js
+++ 07_arrayMethods.js
@@ -79,20 +79,20 @@ arrayOfNums.splice(2, 3, 99, 77 );
 console.log(arrayOfNums);
 
 console.log(`======== join() ==========`);
-var arrayOfNum = [4, 5, 6, 8, 9, 33, 66];
+var arrayOfNums = [4, 5, 6, 8, 9, 33, 66];
 let joinResult = arrayOfNum.join(" ");
 console.log(joinResult);
 console.log(typeof joinResult);
 
 console.log(`======== concat() ==========`);
-var arrayOfNum = [4, 5, 6, 8, 9, 33, 66];
+var arrayOfNums = [4, 5, 6, 8, 9, 33, 66];
 let arrayOfNames = ["Moni", "Jenny", "Shenny", "Tomy"];
 let arrayOfCities = ["Pune", "Mumbai", "Banglore"];
 const concatArray = arrayOfNum.concat(arrayOfNames, arrayOfCities);
 console.log(concatArray);
 
 console.log(`======== Resize an array ==========`);
-var arrayOfNum = [4, 5, 6, 8, 9, 33, 66];
+var arrayOfNums = [4, 5, 6, 8, 9, 33, 66];
 console.log(arrayOfNum.length);
 arrayOfNum.length = 5;
 console.log(arrayOfNum);
