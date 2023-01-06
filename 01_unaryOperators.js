c2375158dda4dc8abf35d1d10ac6f5e386306a41
Dattabamane <dattatraybamane1805@gmail.com>
2023-01-06 18:06:39 +0530

    unaryOperators
    unaryOperators

---
+++ 01_unaryOperators.js
@@ -0,0 +1,22 @@
+console.log("===========================================Unary operatos================================");
+console.log("1]==================== Increment operatos======================");
+console.log("A]===Using prifix of Increment operator ===== ");
+var num1 = 10;
+var result = ++num1;
+console.log(` Use Increment operator : ${result} `);
+
+console.log("B]===Using Postfix of Increment operator ===== ");
+var result = num1++;
+console.log(` Use Increment operator : ${result} `);
+
+console.log("2]==================== Decrement operatos======================");
+console.log("A]===Using prifix of Decrement operator ===== ");
+var num1 = 10;
+var result = --num1;
+console.log(` Use Decrement operator : ${result} `);
+
+console.log("B]===Using Postfix of Decrement operator ===== ");
+var result = num1--;
+console.log(` Use Decrement operator : ${result} `);
+
+
