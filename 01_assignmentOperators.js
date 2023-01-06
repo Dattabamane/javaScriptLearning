c654119229881b37cf96ce2e006a59b125c3d3e7
Dattabamane <dattatraybamane1805@gmail.com>
2023-01-06 18:07:38 +0530

    assignmentOperators
    assignmentOperators

---
+++ 01_assignmentOperators.js
@@ -0,0 +1,26 @@
+console.log("======================================Assignment Operators================================");
+console.log("1] =========== Compound Addition oprator==============");
+var num1 = 10;
+var num2=5;
+ num1 += num2;
+console.log(`Compound Addition of two Numbers is : ${num1}`);
+
+console.log("2] ===========Compound Substraction oprator==============");
+
+ num1 -= num2;
+console.log(`Compound Substraction of two Numbers is : ${num1}`);
+
+console.log("3] ===========Compound Multiplication oprator==============");
+
+  num1 *= num2;
+console.log(`Compound Multiplication of two Numbers is : ${num1}`);
+
+console.log("4] ===========Compound Division oprator==============");
+
+ num1 /= num2;
+console.log(`Compound Division of two Numbers is : ${num1}`);
+
+console.log("5] ===========Compound Modulus oprator==============");
+
+  num1 %= num2;
+console.log(`Compound Modulus of two Numbers is : ${num1}`);
