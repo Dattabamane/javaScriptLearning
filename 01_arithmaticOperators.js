e30550f6903846956b30046759e3da72ba657085
Dattabamane <dattatraybamane1805@gmail.com>
2023-01-06 18:07:15 +0530

    arithmaticOperators
    arithmaticOperators

---
+++ 01_arithmaticOperators.js
@@ -0,0 +1,31 @@
+console.log("======================================Arithmatic Operators================================");
+console.log("1] ===========Addition oprator==============");
+var num1 = 10;
+var num2=5;
+var addtionResult = num1 + num2;
+console.log(`Addition of two Numbers is : ${addtionResult}`);
+
+console.log("2] ===========Substraction oprator==============");
+
+var substractionResult = num1 - num2;
+console.log(`Substraction of two Numbers is : ${substractionResult}`);
+
+console.log("3] ===========Multiplication oprator==============");
+
+var mulResult = num1 * num2;
+console.log(`Multiplication of two Numbers is : ${mulResult}`);
+
+console.log("4] ===========Exponentiation oprator==============");
+
+var expoResult = num1 ** num2;
+console.log(`Exponentiation of two Numbers is : ${expoResult}`);
+
+console.log("5] ===========Division oprator==============");
+
+var divResult = num1 / num2;
+console.log(`Division of two Numbers is : ${divResult}`);
+
+console.log("6] ===========Modulus oprator==============");
+
+var modulusResult = num1 % num2;
+console.log(`Modulus of two Numbers is : ${modulusResult}`);
