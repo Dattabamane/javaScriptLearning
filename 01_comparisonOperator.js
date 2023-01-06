23619e1da1ea7ed0fd9d350d43f0dbe7b9c0403d
Dattabamane <dattatraybamane1805@gmail.com>
2023-01-06 18:08:08 +0530

    comparisonOperators
    comparisonOperators

---
+++ 01_comparisonOperator.js
@@ -0,0 +1,19 @@
+console.log("===================================Comprison Operators====================================");
+console.log("1]=========Greater than ( > )====================");
+var num1 = 10;
+var num2 = 5;
+var num3 = 10;
+var result = num1 > num2;
+console.log(`10 is greater than 5 : ${result}`);
+
+console.log("2]=========less than ( > )====================");
+var result = num2 < num3;
+console.log(`10 is less than 5 : ${result}`);
+
+console.log("3]=========Greater than or eual to  ( >= )====================");
+var result = num1 >= num3;
+console.log(`10 is Greater than or eual to  10 : ${result}`);
+
+console.log("4]=========less than or eual to  ( >= )====================");
+var result = num1 <= num3;
+console.log(`10 is less than or eual to 10 : ${result}`);
