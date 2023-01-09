7c1123b13081c2057dbf13303b950aac43484197
Dattabamane <dattatraybamane1805@gmail.com>
2023-01-09 21:31:19 +0530

    hoisting
    hoisting

---
+++ 08_hoisting.js
@@ -0,0 +1,17 @@
+city = "pune;"
+console.log(city);
+var city;
+
+let state = "MH";
+console.log(state);
+
+show();
+function show() {
+  console.log("Hello I am in show");
+}
+
+
+// display();
+var display = function () {
+  console.log("Hello I am in display");
+};
