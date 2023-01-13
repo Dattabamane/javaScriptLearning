72dcdf8812103160311474beeafc9addacc4f4ef
Dattabamane <dattatraybamane1805@gmail.com>
2023-01-13 18:41:39 +0530

    modified operator
    modified operator

--- 02_ternaryOpetartor.js
+++ 02_ternaryOpetartor.js
@@ -11,3 +11,21 @@ var sscResult = function(sscMarks){
 }
 var result = sscResult(39);
 console.log(`Result is :", ${result}`);
+
+
+console.log("==============================================================================");
+console.log("Logical operator with teranary operator with nested ternary operator");
+var sscResult = function(sscMarks){
+    // var result = (sscMarks>35 && ! (sscMarks>100)) ? "Pass" : "Fail";
+    // return result;
+   //   var result = (sscMarks>35 && sscMarks<100) ? "Pass" : "Fail";
+   //  return result;
+       var result = (sscMarks<0 || sscMarks>100) ? "in valid data" : (sscMarks>35 ? "pass" : "fail");
+       return result;
+}
+
+console.log(`Result is 23 : ${sscResult(23)}`);
+console.log(`Result is 45 : ${sscResult(45)}`);
+console.log(`Result is 1200 : ${sscResult(1200)}`);
+console.log(`Result is "ten" : ${sscResult("ten")}`);
+console.log(`Result is -1 : ${sscResult(-1)}`);
